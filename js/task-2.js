ea7f589a591fda7414158593745002bb7d3d4f88
MaksymPidhornyi <maksym.ldc@gmail.com>
2025-10-26 19:47:53 +0200

    tasks 1-3 done

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+class Storage {
+  #items = [];
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(item) {
+    return this.#items.push(item);
+  }
+  removeItem(item) {
+    this.#items = this.#items.filter(i => i !== item);
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
